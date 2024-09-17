d145c767a0da77550b43a35c250e1b17a3769c0e
Brandon L. Canete <brandoncanete35@gmail.com>
2024-09-17 18:01:25 +0800

    1displayhelloworld

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,11 @@ import {RouterModule, Routes} from '@angular/router';
 import {CommonModule} from "@angular/common";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {AppComponent} from "./app.component";
-import { NgModel } from '@angular/forms';
 import {provideAnimationsAsync} from "@angular/platform-browser/animations/async";
-import { HomepageComponent } from './components/homepage/homepage.component';
-import { LoginComponent } from './components/login/login.component';
-import { SignUpComponent } from './components/sign-up/sign-up.component';
-import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { DisplayhelloworldComponent } from './fiftyComponents/displayhelloworld/displayhelloworld.component';
 
 const routes: Routes = [
-    {path: '', component: HomepageComponent},
-    {path: 'login', component: LoginComponent},
-    {path: 'sign-up', component: SignUpComponent},
-    {path: 'landing-page', component: LandingPageComponent},
+    {path: 'displayhelloworld', component: DisplayhelloworldComponent},
 ];
 
 @NgModule({
@@ -27,9 +20,6 @@ const routes: Routes = [
     ], 
     declarations: [
         AppComponent,
-        LoginComponent,
-        SignUpComponent,
-        LandingPageComponent,
     ],
     providers: [
         provideAnimationsAsync(),
